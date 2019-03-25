90a79266a063c1a69da8336037cf427fae11fc81
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-03-25 19:11:38 -0300

    POST Cliente

--- src/main/java/com/teste/reservasapi/resource/ClienteResource.java
+++ src/main/java/com/teste/reservasapi/resource/ClienteResource.java
@@ -1,15 +1,18 @@
 package com.teste.reservasapi.resource;
 
+import com.teste.reservasapi.event.RecursoCriadoEvent;
 import com.teste.reservasapi.model.Cliente;
 import com.teste.reservasapi.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/clientes")
@@ -17,6 +20,9 @@ public class ClienteResource {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
 
     @GetMapping
     public Page<Cliente> findAll(Pageable pageable) {
@@ -28,6 +34,15 @@ public class ClienteResource {
         Cliente cliente = clienteRepository.findOne(id);
 
         return cliente != null ? ResponseEntity.ok(cliente) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> save(@Valid @RequestBody Cliente cliente, HttpServletResponse response) {
+        Cliente clienteSalvo = clienteRepository.save(cliente);
+
+        eventPublisher.publishEvent(new RecursoCriadoEvent(this, response, clienteSalvo.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
     }
 
 }

3f38b4f06b32c323cbf86903076d291ba9a7e54a
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-03-25 18:56:24 -0300

    Created event listner for added header location by created objects

---
+++ src/main/java/com/teste/reservasapi/event/RecursoCriadoEvent.java
@@ -0,0 +1,23 @@
+package com.teste.reservasapi.event;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Getter
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+    private static final long serialVersionUID = 1L;
+
+    private HttpServletResponse response;
+    private Long id;
+
+    public RecursoCriadoEvent(Object source, HttpServletResponse response, Long id) {
+        super(source);
+
+        this.response = response;
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/com/teste/reservasapi/event/listner/RecursoCriadoListner.java
@@ -0,0 +1,27 @@
+package com.teste.reservasapi.event.listner;
+
+import com.teste.reservasapi.event.RecursoCriadoEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+
+@Component
+public class RecursoCriadoListner implements ApplicationListener<RecursoCriadoEvent> {
+
+    @Override
+    public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+        HttpServletResponse response = recursoCriadoEvent.getResponse();
+        Long id = recursoCriadoEvent.getId();
+
+        adicionarHeaderLocation(response, id);
+    }
+
+    private void adicionarHeaderLocation(HttpServletResponse response, Long id) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(id).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+    }
+
+}

81914f518a7e5efb23da60ec57a3007bf7032300
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-03-25 18:45:29 -0300

    findOne by id Cliente

--- src/main/java/com/teste/reservasapi/resource/ClienteResource.java
+++ src/main/java/com/teste/reservasapi/resource/ClienteResource.java
@@ -5,7 +5,9 @@ import com.teste.reservasapi.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,13 @@ public class ClienteResource {
     @GetMapping
     public Page<Cliente> findAll(Pageable pageable) {
         return clienteRepository.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findOne(@PathVariable Long id) {
+        Cliente cliente = clienteRepository.findOne(id);
+
+        return cliente != null ? ResponseEntity.ok(cliente) : ResponseEntity.notFound().build();
     }
 
 }
